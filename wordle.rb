488121d28506ba449108cdbd37f2c414373f39c6
Benjamin Stein <ben@benjaminste.in>
2022-01-13 07:24:41 -0800

    WIP scores

--- wordle.rb
+++ wordle.rb
@@ -32,12 +32,13 @@ def valid_word?(word)
 end
 
 class Score
-  SCORE_FILE = 'scores.txt'
+  SCORE_FILE = 'scores.json'
+  MAX_GUESSES = 6
   # [0,0,3,2,2,0]
   attr_accessor :name, :num_games, :scores
   
-  def self.initialize(name)
-    
+  def initialize(name)
+    @name = name
   end
   
   def print
@@ -45,23 +46,57 @@ class Score
   end
   
   def add_result(score)
-    num_names +=1
-    scores[score-1] += 1 if score < MAX_GUESSES
+    @num_games +=1
+    @scores[score-1] += 1 if score < MAX_GUESSES
+  end
+
+  def wins
+    @scores.sum
   end
   
+  def losses
+    @num_games - wins
+  end
+
+  def points
+    (@scores[1-1] * 8) + 
+    (@scores[2-1] * 6) + 
+    (@scores[3-1] * 4) + 
+    (@scores[4-1] * 3) + 
+    (@scores[5-1] * 2) +
+    (@scores[6-1] * 1)
+  end
+
+  # data[@name] = {
+  #   # :name => @name,
+  #   :num_games => @num_games,
+  #   :scores => @scores
+  # }
+  
   def save!
-    data[@name] = {
-      # :name => @name,
-      :num_games => @num_games,
-      :scores => @scores
-    }
     File.write(SCORE_FILE)
   end
   
   def self.open_scores
-    @results = JSON.read(SCORE_FILE)
+    data = JSON.parse(File.read(SCORE_FILE))
+    scores = data.map do |k,v|
+      Score.from_json(k,v)
+    end
+    # @results = JSON.read()
   end
+  
+  def self.from_json(name, data)
+    score = Score.new(name)
+    score.num_games = data['num_games']
+    score.scores = data['scores']
+    score
+  end
+  
 end
+#scores = Score.open_scores
+#s = Score.open_scores[0]
+#s.add_result(1)
+
 
 #ENTER WORD HERE
 puts "Enter your word between #{MIN_LENGTH} and #{MAX_LENGTH} letters here and press enter, or just press enter for automatic picking"
